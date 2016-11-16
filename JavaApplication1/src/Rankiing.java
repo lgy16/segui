21d8493a06f028c45f36e5e42ee820729b5986e6
Lee Gayoung <youngi2915@gmail.com>
2016-11-17 03:19:06 +0900

    ranking button event added

--- JavaApplication1/src/Rankiing.java
+++ JavaApplication1/src/Rankiing.java
@@ -10,12 +10,12 @@
  *
  * @author LGY
  */
-public class Lanking extends javax.swing.JFrame {
+public class Rankiing extends javax.swing.JFrame {
 
     /**
      * Creates new form lanking
      */
-    public Lanking() {
+    public Rankiing() {
         initComponents();
     }
 
@@ -155,21 +155,23 @@ public class Lanking extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Lanking.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Rankiing.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Lanking.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Rankiing.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Lanking.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Rankiing.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Lanking.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Rankiing.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Lanking().setVisible(true);
+                new Rankiing().setVisible(true);
             }
         });
     }
